2081b1abb839cfac3b255a63ada0924da2bd69ee
htinlinnzaw <htinlinnzaw.dev@gmail.com>
2020-10-27 21:07:27 +0630

    1. add: functional function and 2. add: un-fictional function

--- main.py
+++ main.py
@@ -3,6 +3,19 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
+a = 0
+
+
+# un-functional function
+def increment():
+    global a
+    a += 1
+
+
+# functional function
+def increment_function(b):
+    return b + 1
+
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -12,5 +25,8 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    increment()
+    print_hi(a)
+    print_hi(increment_function(0))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
