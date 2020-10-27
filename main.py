8709dd8723b66ec92d4edd8dce6cbcf6c26781a5
htinlinnzaw <htinlinnzaw.dev@gmail.com>
2020-10-27 21:29:30 +0630

    1. add: __init__ method to Orange class

--- main.py
+++ main.py
@@ -25,8 +25,14 @@ def print_hi(name):
 class Orange:
     print("Orange created")
 
+    def __init__(self):
+        self.color = "orange"
+        self.weight = 10
+
     def print_orange(self):
         print(self)
+        print(self.color)
+        print(self.weight)
 
 
 if __name__ == '__main__':
